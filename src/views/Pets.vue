b30d0701ef9ba54170ca00e5b1cc22dc101041f1
Alex McKinnon <alex.r.mckinnon@gmail.com>
2019-10-13 23:04:20 -0700

    Get pets data from state

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -1,25 +1,29 @@
 <template>
     <div>
-        <Pet v-for="(pet, index) in getPets" v-bind="pet" :key="index" />
+        <h1>Pets of REW</h1>
+        <Pet v-for="(pet, index) in pets" v-bind="pet" :key="index" />
     </div>
 </template>
 
 <script>
 import Pet from '@/components/Pet.vue';
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
     components: {
         Pet
     },
     data: function() {
         return {
-            pets: []
         }
     },
     computed: {
         ...mapGetters([
             'getPets'
-        ])
+        ]),
+        pets() {
+            return this.getPets;
+        }
     }
 }
 </script>
