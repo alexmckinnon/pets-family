f276117cac800b84a2e8c32afc054fced401e55c
Alex McKinnon <alex.r.mckinnon@gmail.com>
2019-10-24 00:32:57 -0700

    Add hover to Pet card - shows pet type icon

--- src/components/Pet.vue
+++ src/components/Pet.vue
@@ -1,17 +1,43 @@
 <template>
     <div>
-        <div class="rounded-lg overflow-hidden text-white shadow-lg" :class="bgColor">
-            <img class="mx-auto" :src="image" >
-            <div class="px-3 pt-2">
-                <div class="pet__name font-bold text-xl mb-2">{{ name }}</div>
+        <div class="pet__body rounded-lg overflow-hidden text-white shadow-lg cursor-pointer">
+            <img class="mx-auto w-full" :src="image" >
+            <div class="pet__info px-3 py-1 rounded-b-lg" :class="bgColor">
+                <div class="pet__name font-bold text-xl">{{ name }}</div>
+                <p class="pet__details">
+                    <font-awesome-icon :icon="type" class="text-xl mx-2" />
+                </p>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+.pet__info {
+    position: absolute;
+    bottom: 0;
+    opacity: 0.9;
+    max-height: 40px;
+    width: 100%;
+    overflow: hidden;
+    transition: max-height 0.5s ease-in-out;
+
+}
+
 .pet__name {
     font-family: 'Happy Monkey', cursive;
+}
+
+.pet__details{
+    display: none;
+}
+
+.pet__body:hover .pet__info{
+    max-height: 100%;
+}
+
+.pet__body:hover .pet__details{
+    display: inline;
 }
 </style>
 
